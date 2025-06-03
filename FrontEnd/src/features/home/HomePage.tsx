180f844d4e1857b6ba5809bb8e3d52d7b7824518
Ruan Gobira <ruangobira@gmail.com>
2025-06-02 21:41:53 -0300

    ajustes

--- FrontEnd/src/features/home/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <>
-      <h1>Seja bem vindo à Home</h1>
-      <p>Conteudo</p>
-    </>
-  );
-}
--- FrontEnd/src/routes/index.tsx
+++ FrontEnd/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "../features/login/LoginPage";
 import MainLayout from "../layouts/MainLayout";
-import HomePage from "../features/home/HomePage";
 import DelishareMenu from "../features/DelishareMenu";
 import About from "../features/About";
 
@@ -10,11 +9,12 @@ export default function AppRoutes() {
     <>
       <Routes>
         <Route path="/*" element={<Navigate to="/Delishare" replace />} />
+        <Route path={"/home"} element={<Navigate to="/Delishare" replace />} />
         <Route path={"/login"} element={<LoginPage />} />
 
         <Route element={<MainLayout />}>
           <Route path="/Delishare" element={<DelishareMenu />} />
-          <Route path="/home" element={<HomePage />} />
+
           <Route path="/sobre" element={<About />} />
         </Route>
       </Routes>
